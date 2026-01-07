25ab785e009e79cb4257faf1c5d31f31ea0aa8b6
25KGhost <abdalmoez.anis@gmail.com>
2026-01-07 21:43:22 +0100

    Update config.js

--- api/config.js
+++ api/config.js
@@ -2,5 +2,7 @@ export default function handler(request, response) {
   response.status(200).json({
     supabaseUrl: process.env.SUPABASE_URL,
     supabaseKey: process.env.SUPABASE_ANON_KEY,
+    cloudinaryName: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinaryPreset: process.env.CLOUDINARY_PRESET
   });
 }
